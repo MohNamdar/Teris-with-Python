d0915919970cf3ea0dd408fef92dbd4811daae52
Mohammad <MohNamdar01@gmail.com>
2023-12-25 20:37:45 +0330

    Coordinates bug fixed (all shapes matches block to gather)

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from time import sleep
 import os
 import random
 
-playground = [[' ' for i in range(WIDTH)] for j in range(HEIGHT)]
+playground = [['*' for i in range(WIDTH)] for j in range(HEIGHT)]
 colors = [['white' for i in range(WIDTH+2)] for j in range(HEIGHT+2)]
 
 
 class Shapes:
     def __init__(self):
         # define shapes Coordinates
-        self.all_shape = [[[W_CENTER - 1], [W_CENTER - 3, W_CENTER - 1, W_CENTER + 1]],
+        self.all_shape = [[[W_CENTER], [W_CENTER - 2, W_CENTER, W_CENTER + 2]],
                           [[W_CENTER - 2, W_CENTER], [W_CENTER - 2, W_CENTER]],
-                          [[W_CENTER - 3, W_CENTER - 1], [W_CENTER - 1, W_CENTER + 1]],
+                          [[W_CENTER - 4, W_CENTER - 2], [W_CENTER - 2, W_CENTER]],
                           [[W_CENTER - 4, W_CENTER - 2, W_CENTER, W_CENTER + 2]]]
 
     def random(self):
@@ -22,6 +22,16 @@ class Shapes:
 
 
 s = Shapes()
-shape, rand_num = s.random()
 
-show.fall_shape(playground, shape, colors, rand_num)
+while True:
+    shape = s.all_shape[0]
+    show.fall_shape(playground, shape, colors, 0)
+
+    shape = s.all_shape[1]
+    show.fall_shape(playground, shape, colors, 1)
+
+    shape = s.all_shape[2]
+    show.fall_shape(playground, shape, colors, 2)
+
+    shape = s.all_shape[3]
+    show.fall_shape(playground, shape, colors, 3)
